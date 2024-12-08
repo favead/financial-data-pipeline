7582392358f5cc40d099a63e3c96e2e7e6d279b9
favead <diablo3_monk@mail.ru>
2024-12-09 01:06:13 +0300

    Delete dummy docstrings

--- src/financial_data/preprocessing/pdf2txt.py
+++ src/financial_data/preprocessing/pdf2txt.py
@@ -7,17 +7,6 @@ import pymupdf4llm
 def convert_pdf_to_txt(pdf_file_path: Path, txt_file_path: Path) -> None:
     """
     Convert PDF file to text file in markdown format via pymupdf4llm.
-
-    Parameters:
-    ----------
-    pdf_file_path: Path
-        Path to the PDF file.
-    txt_file_path: Path
-        Path to the text file.
-
-    Returns:
-    -------
-    None
     """
     print(f"Converting {pdf_file_path} to {txt_file_path}")
     with open(txt_file_path, "wb") as f:
@@ -26,22 +15,10 @@ def convert_pdf_to_txt(pdf_file_path: Path, txt_file_path: Path) -> None:
     return None
 
 
-def get_pdf_file(textbook_dir: Path, raw_txt_path: Path) -> Path | None:
+def get_pdf_file(textbook_dir: Path) -> Path | None:
     """
     Extract pdf file from textbook directory.
     It is guaranteed that there is only one pdf file in the textbook directory.
-
-    Parameters:
-    ----------
-    textbook_dir: Path
-        Path to the textbook directory.
-    raw_txt_path: Path
-        Path to the raw text directory.
-
-    Returns:
-    -------
-    Path | None
-        Path to the pdf file if found, None otherwise.
     """
     # Find PDF file in textbook directory
     pdf_files = list(textbook_dir.glob("*.pdf"))
@@ -60,7 +37,7 @@ def pdf2txt() -> None:
     for i, textbook_dir in enumerate(pdf_dir.iterdir()):
         if not textbook_dir.is_dir():
             continue
-        pdf_file = get_pdf_file(textbook_dir, txt_dir)
+        pdf_file = get_pdf_file(textbook_dir)
         if pdf_file is None:
             continue
         txt_file_dir = txt_dir / textbook_dir.name
