2cc00b410cbcf68c31897800f2fb7933be7cdafb
favead <diablo3_monk@mail.ru>
2024-12-23 11:29:32 +0300

    Add prefect pipeline script

---
+++ src/financial_data/main.py
@@ -0,0 +1,25 @@
+from prefect import flow
+from .collect import parse_bcs_courses, parse_tinkoff_courses
+from .preprocessing import (
+    clear_txt,
+    index_chunks,
+    pdf2txt,
+    process_3d_party_data,
+    split_documents,
+)
+
+
+@flow
+def run_pipeline() -> None:
+    parse_bcs_courses(),
+    parse_tinkoff_courses()
+    pdf2txt()
+    clear_txt()
+    process_3d_party_data()
+    split_documents()
+    index_chunks()
+    return None
+
+
+if __name__ == "__main__":
+    run_pipeline()
