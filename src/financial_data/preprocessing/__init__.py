312a8ba8d3c127c58aab830585646c5b645bfac6
favead <diablo3_monk@mail.ru>
2025-01-15 19:27:47 +0300

    Update imports from preprocessing module

--- src/financial_data/preprocessing/__init__.py
+++ src/financial_data/preprocessing/__init__.py
@@ -1,4 +1,6 @@
 from .clear_txt import clear_txt
+from .create_config import create_configs
+from .html2txt import html2txt
 from .index import index_chunks
 from .pdf2txt import pdf2txt
 from .thrd_party import process_3d_party_data
@@ -6,6 +8,8 @@ from .split import split_documents
 
 __all__ = [
     clear_txt,
+    create_configs,
+    html2txt,
     index_chunks,
     pdf2txt,
     process_3d_party_data,
