29e4824c9b130bed9d9f28f2b10020183826996f
favead <diablo3_monk@mail.ru>
2025-01-15 19:03:04 +0300

    Saving txt files to database

--- src/financial_data/preprocessing/html2txt.py
+++ src/financial_data/preprocessing/html2txt.py
@@ -1,8 +1,9 @@
-import os
 from pathlib import Path
 
 import html2text
-from prefect import flow
+from prefect import flow, task
+
+from ..storages import initialize_storage, DocumentStorage
 
 
 @flow
@@ -11,22 +12,29 @@ def html2txt() -> None:
     Convert html files to txt with md format
     """
     html_data_dirs = Path("./data/courses")
-    output_dir = Path("./data/txt_data")
+    document_storage = initialize_storage("document")
+
     h = html2text.HTML2Text()
     h.ignore_links = True
     h.ignore_images = True
+
     for data_dir in html_data_dirs.iterdir():
-        output_data_dir = output_dir / data_dir.name
-        os.makedirs(output_data_dir, exist_ok=True)
         for file in data_dir.iterdir():
             if not (file.suffix == ".html" or file.suffix == ".txt"):
                 continue
             with open(file, "r") as f:
                 content = f.read()
             transformed = h.handle(content)
-            output_path = output_data_dir / file.stem
-            with open(f"{output_path}.txt", "w") as f:
-                f.write(transformed)
+            source_name = data_dir.name
+            save_to_storage(document_storage, source_name, transformed)
+
+
+@task
+def save_to_storage(
+    document_storage: DocumentStorage, source_name: str, document: str
+) -> None:
+    document_storage.set_raw_document(source_name, document)
+    return None
 
 
 if __name__ == "__main__":
